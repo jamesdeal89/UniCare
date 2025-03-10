37a27ef5b9774f09faa4c8a7ea43c777ab48ab43
Sammy Sahnine <sammysahnine0@gmail.com>
2025-03-10 16:22:51 +0000

    Added login button to the login screen

--- team_31_health_app/lib/views/login/loginView.dart
+++ team_31_health_app/lib/views/login/loginView.dart
@@ -8,48 +8,57 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-  final TextEditingController userNameTextEditingController = TextEditingController();
-  final TextEditingController passWordTextEditingController = TextEditingController();
+  final TextEditingController userNameTextEditingController =
+      TextEditingController();
+  final TextEditingController passWordTextEditingController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(  
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
             Padding(
-              padding: EdgeInsets.all(20),
-              child: Image(image: AssetImage('assets/images/Nottingham_Blue_white_text_logo_SCREEN.png'),)
-            ),
+                padding: EdgeInsets.all(20),
+                child: Image(
+                  image: AssetImage(
+                      'assets/images/Nottingham_Blue_white_text_logo_SCREEN.png'),
+                )),
             Padding(
-              padding: EdgeInsets.all(20),
-              child: TextField(
-                controller: userNameTextEditingController, 
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(), 
-                  labelText: "Username",
-                  fillColor: Theme.of(context).colorScheme.primary,
-                  filled: true
-                ),
-              )
-            ),
-            
+                padding: EdgeInsets.all(20),
+                child: TextField(
+                  controller: userNameTextEditingController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Username",
+                      fillColor: Theme.of(context).colorScheme.primary,
+                      filled: true),
+                )),
             Padding(
-              padding: EdgeInsets.all(20),
-              child: TextField(  
-                controller: passWordTextEditingController, 
-                obscureText: true, 
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(), 
-                  labelText: "Password",
-                  fillColor: Theme.of(context).colorScheme.primary,
-                  filled: true
-                ),
-              )
-            )
-          ]
-        )
-      ),
+                padding: EdgeInsets.all(20),
+                child: TextField(
+                  controller: passWordTextEditingController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Password",
+                      fillColor: Theme.of(context).colorScheme.primary,
+                      filled: true),
+                )),
+            Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 16.0),
+                child: Center(
+                    child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Theme.of(context).colorScheme.secondary,
+                    backgroundColor: Theme.of(context).colorScheme.onSecondary,
+                  ),
+                  onPressed: () {},
+                  child: Text("Login"),
+                )))
+          ])),
     );
   }
 }
