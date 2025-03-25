6269cfae94a7e138f132490dee1267c0e5ec0339
Sammy Sahnine <sammysahnine0@gmail.com>
2025-03-25 12:09:28 +0000

    Started developing chat interface

--- team_31_health_app/lib/views/main/mainView.dart
+++ team_31_health_app/lib/views/main/mainView.dart
@@ -1,44 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:team_31_health_app/views/main/subviews/chatView.dart';
 
 class MainView extends StatefulWidget {
-  const MainView({Key? key}) : super(key: key);
+  const MainView({super.key});
 
   @override
   State<MainView> createState() => _MainView();
 }
 
+//TODO: Add functionality with switching views with the bottom navigation bar
+
 class _MainView extends State<MainView> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 5,
-      child: Scaffold(
-        bottomNavigationBar: TabBar(
-          tabs: [
-            Tab(
-              icon: Icon(Icons.book),
-              text: "Journal",
-            ),
-            Tab(
-              icon: Icon(Icons.healing),
-              text: "Help",
-            ),
-            Tab(
-              icon: Icon(Icons.chat_bubble),
-              text: "Chat",
-            ),
-            Tab(
-              icon: Icon(Icons.games),
-              text: "Games",
-            ),
-            Tab(
-              icon: Icon(Icons.person),
-              text: "Profile",
-            )
-          ],
-          labelColor: Colors.grey,
-          unselectedLabelColor: Colors.white,
-        ),
+    return Scaffold(
+      body: ChatView(),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: "Journal",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.healing),
+            label: "Help",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble),
+            label: "Chat",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.games),
+            label: "Games",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          )
+        ],
       ),
     );
   }
