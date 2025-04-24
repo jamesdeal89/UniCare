3ce25e807513f256e7b98dba1261a30c04fcd4af
psyaj9 <athikashjeyaganthan@yahoo.com>
2025-04-24 10:55:06 +0100

    Re-adding database files for journal

---
+++ team_31_health_app/lib/data/database/journalRepo.dart
@@ -0,0 +1,96 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:team_31_health_app/data/database/databaseService.dart';
+import 'package:team_31_health_app/data/database_fields/journalEntry.dart';
+
+class JournalRepo extends DatabaseService<JournalEntry> {
+  JournalRepo({required super.database});
+
+  static const String tableName = 'journal_entries';
+
+  static const String columnId = 'id';
+  static const String columnTitle = 'title';
+  static const String columnDate = 'date';
+  static const String columnDescription = 'description';
+  static const String columnGive = 'give';
+  static const String columnTakeNotice = 'takeNotice';
+  static const String columnKeepLearning = 'keepLearning';
+  static const String columnBeActive = 'beActive';
+  static const String columnConnect = 'connect';
+
+
+  @override
+  Future<JournalEntry> insert(JournalEntry data) async {
+    running = true;
+    try {
+      await database.insert(
+        tableName,
+        data.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+      running = false;
+      return data;
+    } on Exception catch (_) {
+      running = false;
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<JournalEntry>> get() async {
+    running = true;
+    try {
+      final List<Map<String, dynamic>> maps = await database.query(tableName);
+      running = false;
+      return List.generate(maps.length, (i) {
+        return JournalEntry.fromMap(maps[i]);
+      });
+    } on Exception catch (_) {
+      running = false;
+      rethrow;
+    }
+  }
+
+  Future<JournalEntry> update(JournalEntry data) async {
+    running = true;
+    try {
+      await database.update(
+        tableName,
+        data.toMap(),
+        where: '$columnId = ?',
+        whereArgs: [data.id],
+      );
+      running = false;
+      return data;
+    } on Exception catch (_) {
+      running = false;
+      rethrow;
+    }
+  }
+
+  Future<void> delete(String id) async {
+    running = true;
+    try {
+      await database.delete(
+        tableName,
+        where: '$columnId = ?',
+        whereArgs: [id],
+      );
+      running = false;
+    } on Exception catch (_) {
+      running = false;
+      rethrow;
+    }
+  }
+
+
+  Future<void> clear() async {
+    running = true;
+    try {
+      await database.delete(tableName);
+      running = false;
+    } catch (e) {
+      running = false;
+      rethrow;
+    }
+  }
+} 
---
+++ team_31_health_app/lib/data/database_fields/journalEntry.dart
@@ -0,0 +1,61 @@
+import 'package:team_31_health_app/data/database_fields/data_model.dart';
+
+class JournalEntry implements DataModel {
+  String title;
+  DateTime date;
+  String description;
+  final String id;
+  
+  final bool give;
+  final bool takeNotice;
+  final bool keepLearning;
+  final bool beActive;
+  final bool connect;
+
+  JournalEntry({
+    required this.title,
+    required this.date,
+    required this.description,
+    String? id,
+    this.give = false,
+    this.takeNotice = false,
+    this.keepLearning = false,
+    this.beActive = false,
+    this.connect = false,
+  }) : id = id ?? DateTime.now().millisecondsSinceEpoch.toString(); 
+
+  @override
+  Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'date': date.toIso8601String(), 
+      'description': description,
+      'give': give ? 1 : 0,
+      'takeNotice': takeNotice ? 1 : 0,
+      'keepLearning': keepLearning ? 1 : 0,
+      'beActive': beActive ? 1 : 0,
+      'connect': connect ? 1 : 0,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'JournalEntry{id: $id, title: $title, date: $date, description: $description, give: $give, takeNotice: $takeNotice, keepLearning: $keepLearning, beActive: $beActive, connect: $connect}';
+  }
+
+
+  static JournalEntry fromMap(Map<String, dynamic> map) {
+    return JournalEntry(
+      id: map['id'] as String,
+      title: map['title'] as String,
+      date: DateTime.parse(map['date'] as String), 
+      description: map['description'] as String,
+      give: map['give'] == 1,
+      takeNotice: map['takeNotice'] == 1,
+      keepLearning: map['keepLearning'] == 1,
+      beActive: map['beActive'] == 1,
+      connect: map['connect'] == 1,
+    );
+  }
+} 
