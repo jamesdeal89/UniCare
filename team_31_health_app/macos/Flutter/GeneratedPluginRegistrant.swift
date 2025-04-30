15d5545cd3a7f447fe593ee627399b9778b3451f
Chinonso Oti <psyco1@nottingham.ac.uk>
2025-04-30 22:27:32 +0100

    dont push

--- team_31_health_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ team_31_health_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,10 +8,7 @@ import Foundation
 import firebase_auth
 import firebase_core
 import sqflite_darwin
-<<<<<<< HEAD
-=======
 import url_launcher_macos
->>>>>>> 2b39a9f18a7d9cd7ef200e0a60649cbf167af4a1
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
